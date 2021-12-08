d0e8042635553c4b7eb7e2a2a0a1369d3fd16749
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2021-12-08 10:20:27 -0500

    update files

--- mcts.py
+++ mcts.py
@@ -5,3 +5,41 @@ Created on Mon Dec  6 20:59:35 2021
 @author: gyshi
 """
 
+import random
+import numpy as np
+import networkx as nx
+import copy
+import sys
+from simulator import State
+from simulator import Simulator
+
+class DefaultPolicy:
+    def __init__(self):
+        next
+
+class MctsPolicy:
+    def __init__(self, root_state):
+        # initialize a sample search tree
+        self.tree = nx.DiGraph()
+        # Prevents division by 0 in calculation of UCT
+        self.EPSILON = 10e-6
+        # UCB coefficient
+        self.uct_c = np.sqrt(2)
+        self.simulator = Simulator()
+        # N is the number of times that this state has been visited
+        # Na is a dictionary to track the the number of times of (s, a) 
+        # Vc is the value for cost, Qr is Q-value for reward, Qc is for cost
+        
+        actions = self.simulator.actions(root_state)
+        action_dict = dict(zip(actions, np.zeros(actions.shape)))
+        
+        self.tree.add_node((root_state, 0), N = 0, Na = action_dict, 
+                           Vc=0, Qr=0, Qc=0)
+        
+        
+        
+        
+        
+        
+    
+    
--- simulator.py
+++ simulator.py
@@ -4,4 +4,33 @@ Created on Mon Dec  6 21:00:50 2021
 
 @author: gyshi
 """
+import numpy as np
+import networkx as nx
 
+
+class State:
+    def __init__(self):
+        # use (x, y) to represent the state
+        self.state = np.array([0,0]) 
+    
+    # State needs to be hashable so that it can be used as a unique graph
+    # node in NetworkX
+    def __key(self):
+        return self.__str__()
+    
+    def __eq__(x, y):
+        return x.__key() == y.__key()
+    
+    def __hash__(self):
+        return hash(self.__key())
+    
+    def __str__(self):                                                                                                          
+        return np.array2string(self.state)
+
+class Simulator:
+    def __init__(self):
+        next
+    
+    def actions(self, state):
+        # return available actions in state, represented in a numpy array
+        return
