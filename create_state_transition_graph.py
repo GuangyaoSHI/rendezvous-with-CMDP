23d622afa7e112da3b24077176c57e6f13d3c7bf
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2022-01-31 17:25:57 -0500

    Update create_state_transition_graph.py

--- create_state_transition_graph.py
+++ create_state_transition_graph.py
@@ -5,3 +5,16 @@ Created on Fri Jan 28 16:44:15 2022
 @author: gyshi
 """
 
+
+import csv
+
+file = open("Coords.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
