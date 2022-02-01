0b36bd9f61f82781c587f956050b02c0a8a20ff3
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2022-02-01 17:32:54 -0500

    verified in matlab

--- create_state_transition_graph.py
+++ create_state_transition_graph.py
@@ -7,6 +7,11 @@ Created on Fri Jan 28 16:44:15 2022
 
 
 import csv
+import networkx as nx
+import numpy as np
+
+def distance(a1, a2):
+    return np.linalg.norm(a1-a2)
 
 file = open("Coords.csv")
 csvreader = csv.reader(file)
@@ -17,4 +22,43 @@ rows = []
 for row in csvreader:
     rows.append(row)
 print(rows)
+
+names = []
+G = nx.Graph()
+for i in range(0, len(rows)):
+    if i < 7:
+        G.add_node(rows[i][0], pos=np.array([float(rows[i][1]), float(rows[i][2])]))  
+        names.append(rows[i][0])
+    else:
+        G.add_node(i-7, pos=np.array([float(rows[i][1]), float(rows[i][2])]))
+
+
+
+for node in G.nodes:
+    dis = (distance(G.nodes['Depot A']['pos'], G.nodes[node]['pos'])
+    +distance(G.nodes[node]['pos'], G.nodes['AOI A']['pos'])
+    +distance(G.nodes['AOI A']['pos'], G.nodes['Depot B']['pos']))
+    if  18 <= dis <= 19:
+        print("node is {} dis is {}".format(node, dis))
+        print("node position is {}".format(G.nodes[node]['pos']))
+
+
+
+road_network = nx.Graph()
+for i in range(7, len(rows)-1):
+    node1 = (float(rows[i][1]), float(rows[i][2]))
+    node2 = (float(rows[i+1][1]), float(rows[i+1][2]))
+    dis = np.linalg.norm(np.array(node1)-np.array(node2))
+    road_network.add_edge(node1, node2, dis=dis)
+
+((6.29, 11.14), (1.0, 13.4)) in road_network.edges
+road_network.remove_edge((6.29, 11.14), (1.0, 13.4))
+((6.29, 11.14), (17.5, 1.5)) in road_network.edges
+road_network.remove_edge((6.29, 11.14), (17.5, 1.5))
+
+pos = dict(zip(road_network.nodes, road_network.nodes))
+nx.draw(road_network, pos=pos, alpha=1, node_color='r', node_size=20)
+
+
+
 file.close()
--- test_main.m
+++ test_main.m
@@ -1,10 +1,38 @@
-load('paths.mat')
-for i = 1:size(paths, 1)
-    plot_grid_world(7, 7);
-    hold on
-    plot(paths(i, 1)+1, paths(i, 2)+1, 'mo','MarkerSize', 40);
-    title("t = " + string(i))
-    hold off
-    pause(0.5);
+filename = 'Coords.csv';
+M = readmatrix(filename)
+
+M(8,:)
+X = M(8:end, 2);
+Y = M(8:end, 3);
+plot(X, Y, '*-')
+axis equal
+
+paths = [6.8, 19.1;
+         5.83, 16.495;
+         7.35, 14.48;
+         4.44, 13.993;
+         1, 13.4;
+         2.69, 10.62;
+         5.7, 11.45;
+         10, 12;
+         9.72, 9.2;
+         11.243, 7.518;
+         12.507, 6.27;
+         14.076, 4.845;
+         13.61, 1.23;
+         16.322, 3.549;
+         17.5, 1.5;
+        ];
+lengths = [];
+
+for i = 2:size(paths, 1)
+    lengths = [lengths, norm(paths(i, :)-paths(i-1, :))];
 end
 
+disp(lengths)
+x = paths(:, 1);
+y = paths(:, 2);
+
+figure(2)
+plot(x, y, '-*')
+axis equal
