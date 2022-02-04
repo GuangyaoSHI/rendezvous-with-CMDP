6af4807df52a1dcfbf1095410d6c134fe1076cac
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2022-02-04 13:33:41 -0500

    parallel computation

--- extract_P_s_a_11-20.py
+++ extract_P_s_a_11-20.py
@@ -29,18 +29,19 @@ for interval in stats:
     probs_be.append(stats[interval])
 
 # best range velocity
-stats = rendezvous.get_power_consumption_distribution(rendezvous.velocity_uav['v_br'])
-powers_br = []
-probs_br = []
-for interval in stats:
-    powers_br.append((interval[0]+interval[1])/2)
-    probs_br.append(stats[interval])
+# stats = rendezvous.get_power_consumption_distribution(rendezvous.velocity_uav['v_br'])
+# powers_br = []
+# probs_br = []
+# for interval in stats:
+#     powers_br.append((interval[0]+interval[1])/2)
+#     probs_br.append(stats[interval])
 
 
 
 # transition probability
 P_s_a = {}
 
+pickle_name = 'P_s_a_'+'11-20'+'.obj'
 #probs = [2.27e-2, 13.6e-2, 34.13e-2, 34.13e-2, 13.6e-2, 2.27e-2]
 #values = [-0.25, -0.15, -0.05, 0.05, 0.15, 0.25]
 
@@ -51,7 +52,7 @@ state_init = (6.8e3, 19.1e3, 6.8e3, 19.1e3, 100, 0)
 for uav_state in UAV_task.nodes:
     print("uav state {}".format(uav_state))
     for ugv_state in road_network.nodes:
-        for battery in range(0, 101):
+        for battery in range(11, 21):
             # Todo: different node may represent the same node, this can cause some problem
             for ugv_task_node in UGV_task.nodes:
                 # power state
@@ -165,255 +166,7 @@ P_s_a[state_l][action] = {}
 P_s_a[state_l][action][state_l] = 1
 
 
-
-
-# Getting back the objects:
-P_s_a = {}
-files = ['P_s_a_0-10', 'P_s_a_11-20', 'P_s_a_21-30', 'P_s_a_31-40', 'P_s_a_41-50',
-         'P_s_a_51-60', 'P_s_a_61-70', 'P_s_a_71-80', 'P_s_a_81-90', 'P_s_a_91-100']    
-for file in files:
-    filename = file+'.obj'
-    with open(filename, 'rb') as f:  # Python 3: open(..., 'rb')
-        P_s_a__ = pickle.load(f)
-    P_s_a = {**P_s_a, **P_s_a__}
-
 # Saving the state-transition graph:
-with open('P_s_a.obj', 'wb') as f:  # Python 3: open(..., 'wb')
+with open(pickle_name, 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump(P_s_a, f)   
       
-# construct a transition graph
-G = nx.DiGraph()
-for state in P_s_a:
-    G.add_node(state, action=list(P_s_a[state].keys()))
-    for action in P_s_a[state]:
-        G.add_edge(state, state+(action,))
-        for next_state in P_s_a[state][action]:
-            assert next_state in P_s_a
-            G.add_edge(state+(action,), next_state, action=action, prob=P_s_a[state][action][next_state])
-
-# Saving the state-transition graph:
-with open('state_transition_graph.obj', 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump(G, f)
-
-# for node in G:
-#     if node not in P_s_a.keys():
-#         print(node)
-
-# state = (3, 0, 3, 0, 99, 3)
-# neighbors = G.neighbors(state)
-# for neighbor in neighbors:
-#     print([state, neighbor])
-#     print(G.edges[state, neighbor])
-
-i = 1        
-for state in P_s_a:
-    for action in P_s_a[state]:
-        for next_state in P_s_a[state][action]:
-            if next_state == state_f:
-                print("state: {} action:{} prob: {}".format(state, action, P_s_a[state][action][next_state]))
-                i += 1
-                if i>100:
-                    break
-# Saving the objects:
-# with open('P_s_a.obj', 'wb') as f:  # Python 3: open(..., 'wb')
-#     pickle.dump(P_s_a, f)
-
-# Getting back the objects:
-with open('P_s_a.obj', 'rb') as f:  # Python 3: open(..., 'rb')
-    P_s_a = pickle.load(f)
-
-with open('state_transition_graph.obj', 'rb') as f:  # Python 3: open(..., 'rb')
-    G = pickle.load(f)
-                       
-# create transition function 
-def transition_prob(s_a_s):
-    state = tuple(list(s_a_s)[0:6])
-    action = s_a_s[6]
-    next_state = tuple(list(s_a_s)[7:])
-    
-    # unreachable state
-    if (action not in P_s_a[state]) or (next_state not in P_s_a[state][action]):
-        #print("trying to transit to an unreachable state")
-        return 0
-    
-    # failure state
-    if state == state_f:     
-        assert action == 'l' and (next_state == state_l)
-        return 1
-
-    # goal
-    if (state[0], state[1]) == UAV_goal:
-        assert action == 'l' and (next_state == state_l)
-        return 1
-    # loop state
-    if state == state_l:
-        assert action == 'l' and (next_state == state_l)
-        return 1
-    
-    return P_s_a[state][action][next_state] 
-
-
-def reward(s_a):
-    state = tuple(list(s_a)[0:6])
-    action = s_a[6]
-    
-    if state == state_f:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    if state == state_l:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    if (state[0], state[1]) == UAV_goal:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    if action == 'v_be':
-        return -1
-    
-    if action == 'v_be_be':
-        uav_state = state[0:2]
-        uav_state_next = list(UAV_task.neighbors(uav_state))[0]
-        ugv_state = state[2:4]
-        ugv_road_state = ugv_state + ugv_state
-        ugv_task_node = state[-1]
-        v1 = action[0:4]
-        v2 = 'v'+action[4:]
-        uav_state_next = list(UAV_task.neighbors(uav_state))[0]
-        rendezvous_state, t1, t2 = rendezvous.rendezvous_point(uav_state, uav_state_next, ugv_state, 
-                                                               ugv_road_state, ugv_task_node, 
-                                                               rendezvous.velocity_uav[v1], 
-                                                                               rendezvous.velocity_uav[v2])
-        return -(t1+t2+rendezvous.charging_time)
-    
-
-
-def cost(s_a):
-    state = tuple(list(s_a)[0:6])
-    action = s_a[6]
-    
-    if state == state_f:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    if state == state_l:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    if (state[0], state[1]) == UAV_goal:
-        assert action == 'l', "should transit to loop state"
-        return 0
-    
-    # compute cost
-    C = 0
-    for next_state in P_s_a[state][action]:
-        if next_state == state_f:
-            C += P_s_a[state][action][next_state]
-    
-    assert C <= 1
-    
-    return C
-
-
-threshold = 0.01
-model = gp.Model('LP_CMDP')
-
-#indics for variables
-indices = []
-
-for state in P_s_a:
-    # if state == state_l:
-    #     continue
-    for action in P_s_a[state]:
-        indices.append(state + (action,))
-
-# add Non-negative continuous variables that lies between 0 and 1        
-rho = model.addVars(indices, lb=0, ub=1, vtype=GRB.CONTINUOUS, name='rho')
-
-# add constraints 
-# cost constraints
-C = 0
-for s_a in indices:
-    if cost(s_a)>0:
-        print("s_a {} cost is {}".format(s_a, cost(s_a)))
-    C += rho[s_a] * cost(s_a)
-cost_ctrs =  model.addConstr(C <= threshold, name = 'cost_ctrs')    
-
-
-for state in P_s_a:
-    if state == state_l:
-        continue
-    lhs = 0
-    rhs = 0
-    for action in P_s_a[state]:
-        #print(rho[state+(action,)].x)
-        lhs += rho[state+(action,)]
-        
-    # for s_a in indices:
-    #     s_a_s = s_a + state
-    #     rhs += rho[s_a] * transition_prob(s_a_s)
-    # for s in G.predecessors(state):
-    #     action = G.edges[s, state]['action']
-    #     s_a = s + (action,)
-    #     s_a_s = s + (action,) + state
-    #     rhs += rho[s_a] * transition_prob(s_a_s)
-    s_a_nodes = []
-    for s_a in G.predecessors(state):
-        assert len(s_a) == 7
-        if s_a not in s_a_nodes:
-            s_a_nodes.append(s_a)
-            s_a_s = s_a + state
-            rhs += rho[s_a] * transition_prob(s_a_s)
-        
-    if state == state_init:
-        print('initial state delta function')
-        rhs += 1
-    
-    model.addConstr(lhs == rhs, name = str(state))
-
-model.addConstrs((rho[s_a] >=0 for s_a in indices), name='non-negative-ctrs')
-    
-obj = 0
-for s_a in indices:
-    obj += rho[s_a] * reward(s_a)
-    
-model.setObjective(obj, GRB.MAXIMIZE)
-model.optimize()
-
-policy = {}
-for s_a in indices:
-    state = tuple(list(s_a)[0:6])
-    actions = list(P_s_a[state].keys())
-    deno = rho[s_a].x
-    # if rho[s_a].x > 0:
-        #print("s_a: {} rho_s_a: {}".format(s_a, rho[s_a].x ))
-    num = 0
-    for action in actions:
-        num += rho[state+(action, )].x
-    
-    if num == 0:
-        policy[s_a] = 0
-    else:
-        policy[s_a] = deno/num
-        if policy[s_a]>0:
-            print("s_a: {} pi_s_a: {}".format(s_a, policy[s_a]))
-    #print("Optimal Prob of choosing action {} at state {} is {}".format((s_a[2], s_a[3]), state, deno/num))
-        
-# Saving the objects:
-with open('policy.obj', 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump(policy, f)        
-    
-    
-# simulate the whole process
-    
-    
-
-                                
-
-
-
-                    
-
-                    
-            
