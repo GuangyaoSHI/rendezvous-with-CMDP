cb2c18f3881487e6fe3bb451f4aec895f40a559d
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2022-04-06 19:46:26 -0400

    Update 2-LP-CMDP-toy-example-gradient_descent.py

--- 2-LP-CMDP-toy-example-gradient_descent.py
+++ 2-LP-CMDP-toy-example-gradient_descent.py
@@ -38,14 +38,14 @@ P_s_a['s0']['a2']['sg'] = 0.6
 P_s_a['s0']['a2']['sf'] = 0.4
 
 P_s_a['sg'] = {}
-P_s_a['sg']['al'] = {}
-P_s_a['sg']['al']['sl'] = 1
+P_s_a['sg']['l'] = {}
+P_s_a['sg']['l']['sl'] = 1
 P_s_a['sf'] = {}
-P_s_a['sf']['al'] = {}
-P_s_a['sf']['al']['sl'] = 1
+P_s_a['sf']['l'] = {}
+P_s_a['sf']['l']['sl'] = 1
 P_s_a['sl'] = {}
-P_s_a['sl']['al'] = {}
-P_s_a['sl']['al']['sl'] = 1
+P_s_a['sl']['l'] = {}
+P_s_a['sl']['l']['sl'] = 1
 
 G = nx.DiGraph()
 for state in P_s_a:
@@ -60,9 +60,9 @@ for state in P_s_a:
                      
 # create transition function 
 def transition_prob(s_a_s):
-    state = tuple(list(s_a_s)[0])
+    state = s_a_s[0]
     action = s_a_s[1]
-    next_state = tuple(list(s_a_s)[2])
+    next_state = s_a_s[2]
     
     # unreachable state
     if (action not in P_s_a[state]) or (next_state not in P_s_a[state][action]):
@@ -76,7 +76,7 @@ def transition_prob(s_a_s):
         return 1
 
     # goal
-    if state[0] == goal:
+    if state == goal:
         print("reach the goal and transition is {}".format(s_a_s))
         assert action == 'l' and (next_state == state_l)
         return 1
@@ -90,8 +90,8 @@ def transition_prob(s_a_s):
 
 
 def reward(s_a):
-    state = tuple(list(s_a)[0:5])
-    action = s_a[5]
+    state = s_a[0]
+    action = s_a[1]
     
     if state == state_f:
         assert action == 'l', "should transit to loop state"
@@ -101,38 +101,20 @@ def reward(s_a):
         assert action == 'l', "should transit to loop state"
         return 0
     
-    if state[0] == UAV_goal:
+    if state == goal:
         assert action == 'l', "should transit to loop state"
         return 0
     
-    if action in ['v_be', 'v_br']:
-        uav_state = state[0]
-        uav_state_next = list(UAV_task.neighbors(uav_state))[0]
-        duration = UAV_task.edges[uav_state, uav_state_next]['dis'] / rendezvous.velocity_uav[action]
-        return -duration
+    if action == 'a1':
+        return 1
     
-    if action in ['v_be_be', 'v_br_br']:
-        uav_state = state[0]
-        uav_state_next = list(UAV_task.neighbors(uav_state))[0]
-        ugv_state = state[1:3]
-        ugv_road_state = ugv_state + ugv_state
-        ugv_task_node = state[-1]
-        v1 = action[0:4]
-        v2 = 'v'+action[4:]
-        uav_state_next = list(UAV_task.neighbors(uav_state))[0]
-        rendezvous_state, t1, t2 = rendezvous.rendezvous_point(UAV_task.nodes[uav_state]['pos'], 
-                                                               UAV_task.nodes[uav_state_next]['pos'], 
-                                                               ugv_state, 
-                                                               ugv_road_state, ugv_task_node, 
-                                                               rendezvous.velocity_uav[v1], 
-                                                                               rendezvous.velocity_uav[v2])
-        return -(t1+t2+rendezvous.charging_time)
+    if action == 'a2':
+        return 5
     
-
 
 def cost(s_a):
-    state = tuple(list(s_a)[0:5])
-    action = s_a[5]
+    state = s_a[0]
+    action = s_a[1]
     
     if state == state_f:
         assert action == 'l', "should transit to loop state"
@@ -142,7 +124,7 @@ def cost(s_a):
         assert action == 'l', "should transit to loop state"
         return 0
     
-    if state[0] == UAV_goal:
+    if state == goal:
         assert action == 'l', "should transit to loop state"
         return 0
     
@@ -168,7 +150,7 @@ for state in P_s_a:
     # if state == state_l:
     #     continue
     for action in P_s_a[state]:
-        indices.append(state + (action,))
+        indices.append((state,) + (action,))
 
 
 # add Non-negative continuous variables that lies between 0 and 1        
@@ -191,8 +173,8 @@ for state in P_s_a:
     lhs = 0
     rhs = 0
     for action in P_s_a[state]:
-        #print(rho[state+(action,)].x)
-        lhs += rho[state+(action,)]
+        #print(rho[(state,)+(action,)].x)
+        lhs += rho[(state,)+(action,)]
         
     # for s_a in indices:
     #     s_a_s = s_a + state
@@ -200,10 +182,10 @@ for state in P_s_a:
 
     #s_a_nodes = []
     for s_a in G.predecessors(state):
-        assert len(s_a) == 6
+        #assert len(s_a) == 6
         #if s_a not in s_a_nodes:
         #s_a_nodes.append(s_a)
-        s_a_s = s_a + state
+        s_a_s = s_a + (state,)
         rhs += rho[s_a] * transition_prob(s_a_s)
         
     
@@ -227,41 +209,140 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 model.optimize()
 
-policy = {}
-for s_a in indices:
-    state = tuple(list(s_a)[0:5])
-    actions = list(P_s_a[state].keys())
-    # numerical issue, gurobi will sometimes return a very small negative number for zero
-    if rho[s_a].x < 0:
-        deno = 0
-    else:
-        deno = rho[s_a].x
-    # if rho[s_a].x > 0:
-        #print("s_a: {} rho_s_a: {}".format(s_a, rho[s_a].x ))
-    num = 0
-    for action in actions:
-        #assert rho[state+(action, )].x >= 0
-        if rho[state+(action, )].x < 0:
-            num += 0
-        else:
-            num += rho[state+(action, )].x
-        if rho[state+(action, )].x < 0:
-            print("numerical issue !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print(rho[state+(action, )].x)
-    
-    #assert num >= deno
-    if num == 0:
-        policy[s_a] = 0
-    else:
-        policy[s_a] = deno/num
-        if policy[s_a]>0:
-            print("s_a: {} pi_s_a: {}".format(s_a, policy[s_a]))
-    #print("Optimal Prob of choosing action {} at state {} is {}".format((s_a[2], s_a[3]), state, deno/num))
-
 print("objective value is {}".format(obj.getValue()))        
 # Saving the objects:
        
-with open(policy_name, 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump(policy, f)        
+
+
+
+y = {}
+lambda_1 = 0
+lambda_sa = {}
+nu = {}
+C_sa_pri = {}
+C_sa_sec = {} 
+print("start to initialize")
+for state in P_s_a:
+    if state != state_l:
+        nu[state] = 10
+    for action in P_s_a[state]:
+        y[(state,)+(action,)] = 0
+        lambda_sa[(state,)+(action,)]  = 0
+        C_sa_pri[(state,)+(action,)] = reward((state,)+(action,))
+        assert C_sa_pri[(state,)+(action,)] >= 0
+        C_sa_sec[(state,)+(action,)] = cost((state,)+(action,))
+
+
+# update step size
+ay = 0.012
+a_lambda = 0.001
+a_nu = 0.001
+
+
+
+y_current_step = copy.deepcopy(y)
+lambda_1_current_step = lambda_1
+lambda_sa_current_step = copy.deepcopy(lambda_sa)
+nu_current_step = copy.deepcopy(nu)
+C_current_step = np.dot(np.array(list(y_current_step.values())), np.array(list(C_sa_pri.values())))
+
+
+y_last_step = copy.deepcopy(y)
+C_last_step = np.dot(np.array(list(y_last_step.values())), np.array(list(C_sa_pri.values())))-1
+lambda_1_last_step = lambda_1
+lambda_sa_last_step = copy.deepcopy(lambda_sa)
+nu_last_step = copy.deepcopy(nu)
+#Lagrangian_last_step = Lagrangian_current_step - 0.01
+
+
+
+obj_traces = [C_current_step]
+obj_mean = []
+y_traces = [y_current_step]
+y_mean = []
+#Lagrangian_traces = [Lagrangian_current_step]
+print("start to do iterations")
+for j in range(1, 8000):
+    print("iteration {}".format(j))
+    ay = ay/1.0
+    a_lambda = ay
+    a_nu = ay
+    #Lagrangian_last_step = Lagrangian_current_step
+    for s_a in y:
+        # dy
+        state = s_a[0]
+        last_term_dy = 0
+        for state_next in P_s_a[state][s_a[-1]]:
+            if state_next != state_l:
+                last_term_dy += P_s_a[state][s_a[-1]][state_next]*nu_last_step[state_next]
+        
+        if state != state_l:
+            dy = C_sa_pri[s_a] + lambda_1_last_step*C_sa_sec[s_a] - \
+                lambda_sa_last_step[s_a] + nu_last_step[state] - last_term_dy
+        else:
+            dy = C_sa_pri[s_a] + lambda_1_last_step*C_sa_sec[s_a] - lambda_sa_last_step[s_a] - last_term_dy
+        # update y
+        y_current_step[s_a] = y_last_step[s_a] - ay*dy
+        #y_current_step[s_a] = max(y_current_step[s_a], 0)
+        # d lambda_sa
+        d_lambda_sa = -y_last_step[s_a]
+        # update lambda_sa
+        lambda_sa_current_step[s_a] = lambda_sa_last_step[s_a] + a_lambda*d_lambda_sa
+        if lambda_sa_current_step[s_a] < 0:
+            lambda_sa_current_step[s_a] = 0
+        
+    y_traces.append(y_current_step)
+    # d lambda1
+    d_lambda1 = np.dot(np.array(list(y_last_step.values())), np.array(list(C_sa_sec.values()))) - threshold
+    # update lambda1
+    lambda_1_current_step = lambda_1_last_step + a_lambda*d_lambda1
+    if lambda_1_current_step < 0:
+        lambda_1_current_step = 0
+        
+    for state in P_s_a:
+        # d nu
+        if state != state_l:
+            # first term
+            d_nu_first_term = 0
+            for action in P_s_a[state]:
+                d_nu_first_term += y_last_step[(state,)+(action,)]
+            
+            # indicator
+            initial_state = 0
+            if state == state_init:
+                initial_state = 1
+            
+            # last term 
+            d_nu_last_term = 0
+            for s_a in G.predecessors(state):
+                #assert len(s_a) == 6
+                #if s_a not in s_a_nodes:
+                #s_a_nodes.append(s_a)
+                s_a_s = s_a + (state,)
+                d_nu_last_term += y_last_step[s_a] * transition_prob(s_a_s)
+                
+            d_nu = d_nu_first_term - initial_state - d_nu_last_term
+            
+            # update nu
+            nu_current_step[state] = nu_last_step[state] + a_nu*d_nu
     
+    C_current_step = np.dot(np.array(list(y_current_step.values())), np.array(list(C_sa_pri.values())))
+    obj_traces.append(C_current_step)
+    
+    # update last step information
+    #y_last_step = copy.deepcopy(y_current_step)
+    y_last_step = dict(zip(y_last_step.keys(), y_current_step.values()))
+    #lambda_sa_last_step = copy.deepcopy(lambda_sa_current_step)
+    lambda_sa_last_step = dict(zip(lambda_sa_last_step.keys(), lambda_sa_current_step.values()))
+    
+    lambda_1_last_step = lambda_1_current_step
+    nu_last_step = dict(zip(nu_last_step.keys(), nu_current_step.values()))
+    #nu_last_step = copy.deepcopy(nu_current_step)
+
+# post processing
+for i in range(len(obj_traces)):
+    obj_mean.append(np.mean(obj_traces[0:i+1]))
+    
+print("optimal value is {}".format(obj_mean[-1]))       
+plt.plot(obj_mean)
     
